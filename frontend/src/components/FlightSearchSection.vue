549ca9c20b1ce88670218e833893ac2b6c2bc5b7
xitan <xitan486@student.liu.se>
2023-06-01 23:48:27 +0200

    Flight information display is more concise and beautiful, add FlightDataSection component

--- frontend/src/components/FlightSearchSection.vue
+++ frontend/src/components/FlightSearchSection.vue
@@ -2,112 +2,78 @@
   <div class="bg-gray-200 w-1/2 container mx-auto mt-10 px-5 pt-5 pb-10 rounded">
     <div class="bg-white rounded-lg p-6 shadow-lg">
       <form class="space-y-4">
-        <div class="grid grid-cols-5 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
           <div class="flex items-center space-x-2">
             <label class="block text-sm font-bold">Anytime</label>
-            <input @click="anytime = !anytime; anytime && (travelDate = ''); anytime && (returnDate = '')" type="checkbox" v-model="anytime" class="w-4 h-4">
+            <input @click="anytime = !anytime; anytime && (travelDate = ''); anytime && (returnDate = '')" type="checkbox"
+              v-model="anytime" class="w-4 h-4">
           </div>
           <div class="flex items-center space-x-2">
             <label class="block text-sm font-bold">One Way</label>
-            <input @click="oneWay = !oneWay; oneWay && (returnDate = '')" type="checkbox" v-model="oneWay" class="w-4 h-4">
+            <input @click=" oneWay = !oneWay; oneWay && (returnDate = '') " type="checkbox" v-model=" oneWay "
+              class="w-4 h-4">
           </div>
           <div class="flex items-center space-x-2">
-            <label class="block text-sm font-bold">Inspiration Search</label>
-            <input @click="inspirationSearch = !inspirationSearch; inspirationSearch && (destination = '')" type="checkbox" v-model="inspirationSearch" class="w-4 h-4">
+            <label class="block text-sm font-bold">Explore everywhere</label>
+            <input @click=" inspirationSearch = !inspirationSearch; inspirationSearch && (destination = '') "
+              type="checkbox" v-model=" inspirationSearch " class="w-4 h-4">
           </div>
         </div>
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
           <div>
             <label class="block text-sm font-bold">Departure</label>
-            <input type="text" v-model="departure" class="border p-2 rounded" required>
+            <input type="text" v-model=" departure " class="border p-2 rounded" required>
           </div>
           <div>
             <label class="block text-sm font-bold">Destination</label>
-            <input :disabled="inspirationSearch" type="text" v-model="destination" class="border p-2 rounded" required>
+            <input :disabled=" inspirationSearch " type="text" v-model=" destination " class="border p-2 rounded"
+              required>
           </div>
           <div>
             <label class="block text-sm font-bold">Travel Date</label>
-            <input :disabled="anytime" type="date" v-model="travelDate" id="travelDate" class="border p-2 rounded">
+            <input :disabled=" anytime " type="date" v-model=" travelDate " id="travelDate" class="border p-2 rounded">
           </div>
           <div>
             <label class="block text-sm font-bold">Return Date</label>
-            <input :disabled="anytime || oneWay" type="date" v-model="returnDate" id="returnDate"
+            <input :disabled=" anytime || oneWay " type="date" v-model=" returnDate " id="returnDate"
               class="border p-2 rounded">
           </div>
         </div>
-        <div class="grid grid-cols-3 gap-4">
 
-        </div>
         <div class="flex justify-center">
-          <button @click.prevent="handleSubmit" type="submit"
+          <button @click.prevent=" handleSubmit " type="submit"
             class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Search
           </button>
         </div>
       </form>
 
-      <div v-if="cityFlightData.length === 0 && flights.length === 0" class="grid grid-cols-3 gap-4 mt-10">
-        <div v-for="(flight, index) in countryFlightData" :key="index" class="bg-white rounded-lg p-6 shadow-lg">
+      <div v-if=" cityFlightData.length === 0 && flights.length === 0 " class="grid grid-cols-3 gap-4 mt-10">
+        <div v-for="(      flight, index      ) in       countryFlightData      " :key=" index "
+          class="bg-white rounded-lg p-6 shadow-lg">
           <h3 class="text-xl font-bold">{{ flight.CountryNameEnglish }}</h3>
-          <img @click="searchFlightEverywhereDetails(flight.CountryId)" :src="flight.ImageUrl" alt="Country image"
+          <img @click=" searchFlightEverywhereDetails(flight.CountryId) " :src=" flight.ImageUrl " alt="Country image"
             class="clickable-image w-full h-64 mt-4 rounded" />
           <p class="mt-4">Price: {{ flight.Price }} {{ flight.CurrencyId }}</p>
         </div>
       </div>
 
-      <div v-if="cityFlightData.length !== 0 && flights.length === 0" class="grid grid-cols-3 gap-4 mt-10">
-        <div v-for="(flight, index) in cityFlightData" :key="index" class="bg-white rounded-lg p-6 shadow-lg">
+      <div v-if=" cityFlightData.length !== 0 && flights.length === 0 " class="grid grid-cols-3 gap-4 mt-10">
+        <div v-for="(      flight, index      ) in       cityFlightData      " :key=" index "
+          class="bg-white rounded-lg p-6 shadow-lg">
           <h3 class="text-xl font-bold">{{ flight.title }}</h3>
-          <img @click="searchFlights(flight.destinationPlaceId, flight.outboundDepartureDate)" :src="flight.imageUrl"
+          <img @click=" searchFlights(flight.destinationPlaceId, flight.outboundDepartureDate) " :src=" flight.imageUrl "
             alt="Country image" class="clickable-image w-full h-64 mt-4 rounded" />
           <p class="mt-4">Price: {{ flight.price }} {{ flight.currencyId }}</p>
         </div>
       </div>
 
-      <div v-if="flights.length">
-  <div v-for="(flight, index) in flights" :key="index" class="bg-white rounded-lg p-6 shadow-lg mb-4">
-    <h3 class="text-xl font-bold">Flight {{ index + 1 }}</h3>
-    
-    <h4 v-if="flight.legs[1]" class="text-lg font-bold">Outbound Flight:</h4>
-    <p>Departure: {{ flight.legs[0].departure }}</p>
-    <p>Arrival: {{ flight.legs[0].arrival }}</p>
-    <p>Origin: {{ flight.legs[0].origin.name }} - {{ flight.legs[0].origin.display_code }}</p>
-    <p>Destination: {{ flight.legs[0].destination.name }} - {{ flight.legs[0].destination.display_code }}</p>
-    <p>Carrier: {{ flight.legs[0].carriers[0].name }}</p>
-    <p>Duration: {{ flight.legs[0].duration }} minutes</p>
-    <p v-if="flight.legs[0].stop_count == 0">Direct flight</p>
-    <div v-if="flight.legs[0].stop_count > 0">
-      <p>Stops:</p>
-      <ul>
-        <li v-for="(stop, i) in flight.legs[0].stops" :key="i">{{ stop.name }}</li>
-      </ul>
-    </div>
-    
-    <div v-if="flight.legs[1]">
-      <h4 class="text-lg font-bold">Return Flight:</h4>
-      <p>Departure: {{ flight.legs[1].departure }}</p>
-      <p>Arrival: {{ flight.legs[1].arrival }}</p>
-      <p>Origin: {{ flight.legs[1].origin.name }} - {{ flight.legs[1].origin.display_code }}</p>
-      <p>Destination: {{ flight.legs[1].destination.name }} - {{ flight.legs[1].destination.display_code }}</p>
-      <p>Carrier: {{ flight.legs[1].carriers[0].name }}</p>
-      <p>Duration: {{ flight.legs[1].duration }} minutes</p>
-      <p>Stop Count: {{ flight.legs[1].stop_count }}</p>
-      <div v-if="flight.legs[1].stop_count > 0">
-        <p>Stops:</p>
-        <ul>
-          <li v-for="(stop, i) in flight.legs[1].stops" :key="i">{{ stop.name }}</li>
-        </ul>
+      <div v-if=" flights.length " class="container mx-auto px-2 sm:px-0">
+        <FlightDataSection :flights="flights" />
       </div>
     </div>
-    
-    <p class="font-bold mt-4">Total Duration: {{ flight.totalDuration }} minutes</p>
-    <p>Price: {{ flight.price.amount }} SEK</p>
-    <button @click.prevent="saveFlight(flight)">Save Flight</button>
-  </div>
-</div>
 
 
-    </div>
   </div>
 </template>
 
@@ -115,6 +81,7 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import { useAuthStore } from '@/stores/useAuthStore.js';
+import FlightDataSection from '@/components/FlightDataSection.vue';
 
 const anytime = ref(false);
 const oneWay = ref(false);
@@ -176,7 +143,7 @@ const handleSubmit = async () => {
   else {
     await searchFlights(destination.value, travelDate.value, returnDate.value);
   }
-  
+
 };
 
 const searchFlightEverywhereDetails = async (CountryId) => {
@@ -204,20 +171,20 @@ const searchFlightEverywhereDetails = async (CountryId) => {
 const searchFlights = async (destination, date, returnDate) => {
   try {
     const params = {
-        origin: cityID.value,
-        destination: destination,
-        date: date,
-        returnDate: returnDate,
-        oneWay: oneWay.value,
-        currency: 'SEK',
-        countryCode: 'US',
-        market: 'en-US',
-      };
-      if (!oneWay.value) {
-        params.returnDate = returnDate;
-      }
+      origin: cityID.value,
+      destination: destination,
+      date: date,
+      returnDate: returnDate,
+      oneWay: oneWay.value,
+      currency: 'SEK',
+      countryCode: 'US',
+      market: 'en-US',
+    };
+    if (!oneWay.value) {
+      params.returnDate = returnDate;
+    }
 
-    const response = await axios.get('/searchFlights', {params});
+    const response = await axios.get('/searchFlights', { params });
     flights.value = response.data.data;
     console.log(response.data);
   } catch (error) {
@@ -241,4 +208,6 @@ const saveFlight = async (flight) => {
   transform: scale(1.05);
   box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
 }
+
+
 </style>
--- frontend/src/components/flightDataSection.vue
+++ frontend/src/components/flightDataSection.vue
@@ -1,21 +1,68 @@
 <template>
-    <div class="grid grid-cols-3 gap-4 mt-10">
-    <div
-      v-for="(flight, index) in flightData"
-      :key="index"
-      class="bg-white rounded-lg p-6 shadow-lg"
-    >
-      <h3 class="text-xl font-bold">{{ flight.CountryNameEnglish }}</h3>
-      <img :src="flight.ImageUrl" alt="Country image" class="w-full h-64 mt-4 rounded" />
-      <p class="mt-4">Price: {{ flight.Price }} {{ flight.CurrencyId }}</p>
-    </div>
-  </div>
+    <div v-for="( flight, index ) in  flights " :key=" index "
+          class="flight-card my-4 border border-gray-300 rounded-lg p-4">
+          <div class="flex justify-between mb-4">
+            <div>
+              <div class="font-bold text-lg">{{ new Date(flight.legs[0].departure).toLocaleTimeString('en-US', { hour:
+                '2-digit', minute: '2-digit', hour12: false }) }}</div>
+              <div class="text-sm text-gray-500" :title="flight.legs[0].origin.name">{{flight.legs[0].origin.display_code }}</div>
+            </div>
+            <div class="flex flex-col items-center">
+              <div class="text-sm">{{ flight.legs[0].duration }} mins</div>
+              <div class="font-bold">--------------------</div>
+              <div class="text-sm text-green-700">{{ flight.legs[0].stop_count == 0 ? 'Direct' : flight.legs[0].stops[0].name }}</div>
+            </div>
+            <div class="text-right">
+              <div class="font-bold text-lg">{{ new Date(flight.legs[0].arrival).toLocaleTimeString('en-US', { hour:
+                '2-digit', minute: '2-digit', hour12: false }) }}</div>
+              <div class="text-sm text-gray-500" :title="flight.legs[1].destination.name">{{flight.legs[1].destination.display_code }}</div>
+            </div>
+          </div>
+          <div v-if=" flight.legs[1] " class="flex justify-between mb-4">
+            <div>
+              <div class="font-bold text-lg">{{ new Date(flight.legs[1].departure).toLocaleTimeString('en-US', { hour:
+                '2-digit', minute: '2-digit', hour12: false }) }}</div>
+              <div class="text-sm text-gray-500" :title="flight.legs[1].origin.name"> {{flight.legs[1].origin.display_code}}</div>
+            </div>
+            <div class="flex flex-col items-center">
+              <div>{{ flight.legs[1].duration }} mins</div>
+              <div class="font-bold">--------------------</div>
+              <div class="text-sm text-green-700">{{ flight.legs[1].stop_count == 0 ? 'Direct' : flight.legs[1].stops[0].name }}</div>
+            </div>
+            <div class="text-right">
+              <div class="font-bold text-lg">{{ new Date(flight.legs[1].arrival).toLocaleTimeString('en-US', { hour:
+                '2-digit', minute: '2-digit', hour12: false }) }}</div>
+              <div class="text-sm text-gray-500" :title="flight.legs[1].destination.name">{{flight.legs[1].destination.display_code }}</div>
+            </div>
+          </div>
+          <div class="mt-4 flex justify-between items-center">
+            <div>
+              <p class=" mb-2 text-gray-500">Total Duration: {{ flight.totalDuration }} minutes</p>
+            <p class=" text-gray-500">Price: {{ flight.price.amount }} SEK</p>
+            </div>
+            
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
+              @click.prevent=" saveFlight(flight) ">Save Flight</button>
+          </div>
+        </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  flights: Array
+});
 </script>
 
+
 <style lang="scss" scoped>
+.flight-card {
+  border: px solid #e2e8f0;
+  border-radius: 0.5rem;
+  padding: 2.5rem 7rem;
+  background: white;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+}
 
 </style>
